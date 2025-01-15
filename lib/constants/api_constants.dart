564730c84ace68600f2c7ad15f06eb75f1476d37
Abidi Ahmed <abidiahmedmk@gmail.com>
2025-01-15 19:51:47 +0100

    delete pusher

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter_pusher_client/flutter_pusher.dart';
-
 class ApiConstants {
   static const BASE_URL = 'https://order-crm.de/api';
   //static const BASE_URL = 'http://c516354e4fc3.ngrok.io/api';
@@ -13,24 +11,4 @@ Map<String, String> requestHeaders(String token) {
     HttpHeaders.acceptHeader: 'application/json',
     HttpHeaders.authorizationHeader: 'Bearer $token'
   };
-}
-
-FlutterPusher getPusherClient(String token) {
-  PusherOptions options = PusherOptions(
-      encrypted: false,
-      host: '10.0.3.2',
-      cluster: 'ap1',
-      port: 6001,
-      auth: PusherAuth('http://10.0.3.2:8000/api/broadcasting/auth',
-          headers: {'Authorization': 'Bearer $token'}));
-  return FlutterPusher('d71e6cbc448a4eccfeb2', options, enableLogging: true);
-}
-
-void onConnectionStateChange(ConnectionStateChange event) {
-  print(event.currentState);
-  if (event.currentState == 'CONNECTED') {
-    print('connected');
-  } else if (event.currentState == 'DISCONNECTED') {
-    print('disconnected');
-  }
 }
