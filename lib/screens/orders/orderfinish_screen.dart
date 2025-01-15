a9895a33271f9269819272a8836b58b97108a65e
Abidi Ahmed <abidiahmedmk@gmail.com>
2025-01-15 19:14:57 +0100

    change dropdown button

--- lib/screens/orders/orderfinish_screen.dart
+++ lib/screens/orders/orderfinish_screen.dart
@@ -7,7 +7,6 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:material_dropdown_formfield/material_dropdown_formfield.dart';
 
 import 'package:auftrag_mobile/blocs/order/order_bloc.dart';
 
